876be1d2e07be24d328b4df2c469881e93a7af3f
777aker <777aker@gmail.com>
2024-11-16 07:59:29 -0700

    basic window

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
 #include "window/window.hpp"
 
-int main(int argc, char* argv[]) {
-    Window mainwindow("Collisions", 0, 800, 800, NULL);
-    glDisable(GL_DEPTH_TEST);
-    glClearColor((float)midnight.r/255.0, (float)midnight.g/255.0, (float)midnight.b/255.0, 1.0);
+void key(GLFWwindow *windowobj, int key, int scancode, int action, int mods) {
+	if (action == GLFW_RELEASE)
+		return;
 
-    while(!glfwWindowShouldClose(mainwindow.glwindow)) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	switch (key) {
+	case GLFW_KEY_ESCAPE:
+		glfwSetWindowShouldClose(windowobj, 1);
+		break;
+	}
+}
 
-        // check for display errors
+void display_loop(Window *windowobj) {
+	while (!glfwWindowShouldClose(windowobj->glwindow)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// want to see fps
+		glColor3ub(nephritis.r, nephritis.g, nephritis.b);
+		glRasterPos2i(-windowobj->dim * windowobj->asp + 5, windowobj->dim - 5);
+		Print("FPS=%d", windowobj->FramesPerSecond());
+
+		// check for display errors
 		int err = glGetError();
-		if (err) {
+		if (err)
+		{
 			fprintf(stderr, "ERROR: %s [%s]\n", gluErrorString(err), "display");
 		}
 		// swap buffers
 		glFlush();
-		glfwSwapBuffers(mainwindow.glwindow);
+		glfwSwapBuffers(windowobj->glwindow);
 		// get key board events
 		glfwPollEvents();
-    }
+	}
+}
+
+int main(int argc, char *argv[]) {
+	Window mainwindow("Collisions", 0, 800, 800, key);
+	glDisable(GL_DEPTH_TEST);
+	glClearColor((float)midnight.r / 255.0, (float)midnight.g / 255.0, (float)midnight.b / 255.0, 1.0);
+
+	display_loop(&mainwindow);
 }
--- window/window.hpp
+++ window/window.hpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 // different headers depending on what machine you're on
 #ifdef USEGLEW
@@ -19,10 +20,6 @@
 #include <GL/gl.h>
 #endif
 
-extern double dim; // size of the world
-extern double asp; // aspect ratio
-extern bool wasResized; // called resize function
-
 // colors for drawing
 struct color {
 	int r;
@@ -30,22 +27,29 @@ struct color {
 	int b;
 };
 
-const struct color asphalt = { 52, 73, 94 };
-const struct color concrete = { 127, 140, 141 };
-const struct color nephritis = { 39, 174, 96 };
-const struct color midnight = { 44, 62, 80 };
-const struct color emerald = { 46, 204, 113 };
-const struct color sunflower = { 241, 196, 15 };
-const struct color alizarin = { 231, 76, 60 };
+const struct color asphalt = { 52, 73, 94 }; // grey
+const struct color concrete = { 127, 140, 141 }; // light grey
+const struct color nephritis = { 39, 174, 96 }; // dark green
+const struct color midnight = { 44, 62, 80 }; // dark grey
+const struct color emerald = { 46, 204, 113 }; // light green
+const struct color sunflower = { 241, 196, 15 }; // yellow
+const struct color alizarin = { 231, 76, 60 }; // bright red
+
+void Print(const char* format, ...);
 
 class Window {
-	//GLFWwindow* window;
 public:
+	double dim = 100;
+	double asp = 1;
 	Window(const char* title, int sync, int width, int height,
 		void(*key)(GLFWwindow*, int, int, int, int));
+	int FramesPerSecond();
 	~Window();
 	//void startDisplayLoop(void (*display)(Window*)); // this program isn't going to use this
 	GLFWwindow* glwindow;
+private:
+	int fps = 0;
+	int sec0 = 0, fpscount = 0;
 };
 
 #endif
